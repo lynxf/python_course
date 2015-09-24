d668d916aa0bdb1282ca5b1b9d715fccb5d75914
Администратор <aleksandra9122@mail.ru>
2015-09-24 23:10:21 +0300

    py

---
+++ Homework/1-2.py
@@ -0,0 +1,20 @@
+a=str(input())
+p=int(input())
+if a=='утюг':
+    if p%100==11 or p%100==12 or p%100==13 or p%100==14 or p-100==11 or p-100==12 or p-100==13 or p-100==14:
+         print(p,'утюгов')
+    elif p%10==1:
+        print(p,'утюг')
+    elif p%10==2 or p%10==3 or p%10==4:
+        print(p,'утюга')
+    else:
+        print(p,'утюгов')
+else:
+    if p%100==11 or p%100==12 or p%100==13 or p%100==14 or p-100==11 or p-100==12 or p-100==13 or p-100==14:
+         print(p,'ложек')
+    elif p%10==1:
+        print(p,'ложка')
+    elif p%10==2 or p%10==3 or p%10==4:
+        print(p,'ложки')
+    else:
+        print(p,'ложек')
---
+++ Homework/1-5.py
@@ -0,0 +1,9 @@
+d={}
+s = str(input())
+for i in s:
+    if i not in d:
+        d[i] = 1
+    else:
+        d[i] += 1
+for i in sorted(d):
+    print (i, d[i])
