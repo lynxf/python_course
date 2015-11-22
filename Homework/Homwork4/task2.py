7165585c35f1b95f35f2620fcc732ec1162051ee
lynxf <aleksandra9122@mail.ru>
2015-11-22 19:20:14 +0300

    Create task2.py

---
+++ Homework/Homwork4/task2.py
@@ -0,0 +1,30 @@
+with open("file_name", "r") as d:
+    l = d.read()
+    l = l.split("\n")
+    w_yo = 0
+    w_ka = 0
+    w_v = 0
+    for i in l:
+        if "yo" == (i[-2]+i[-1]):
+            w_yo += 1
+        elif "ka" == (i[-2]+i[-1]):
+            w_ka += 1
+        else:
+            w_v += 1
+    d.close()
+
+    def fac(n):
+        if n == 0:
+            return 1
+        return fac(n-1) * n
+
+    if w_yo < 7:
+        numb_yo = w_yo
+    else:
+        numb_yo = 7
+
+    comb_yo = 0
+    for i in range(0, numb_yo):
+        comb_yo += fac(w_yo)//fac(i)
+    best = comb_yo * w_ka * w_v
+    print(best)
