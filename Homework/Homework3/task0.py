06f1c9db9e91c1c80d97c54f6bbfcd76f1d187b4
lynxf <aleksandra9122@mail.ru>
2015-11-22 17:28:26 +0300

    Update task0.py

--- Homework/Homework3/task0.py
+++ Homework/Homework3/task0.py
@@ -1,22 +1,22 @@
 
-    file = open(file_name, "r")
-    songs = []
-    class Song:
-        def __init__(self, name, artist, album, position, year, duration):
-            self.artist = artist
-            self.name = name
-            self.album = album
-            self.position = position
-            self.year = year
-            self.duration = duration
-        def __repr__(self):
-            song = self.name+' '+self.artist+' '+self.album+' '+self.position+' '+self.year+' '+self.duration
-            return(song)
-    for i in file:
-        name, artist, album, position, year, duration = i.split("\t")
-        t = (name, artist, album, position, year, duration)
-        songs.append(t)
-    return(songs)
+class Song:
+    def __init__(self, name, artist, album, position, year, duration):
+        self.artist = artist
+        self.name = name
+        self.album = album
+        self.position = position
+        self.year = year
+        self.duration = duration
+    def __repr__(self):
+        song = self.name+' '+self.artist+' '+self.album+' '+self.position+' '+self.year+' '+self.duration
+        return(song)
+    
+    def __lt__(self, other):
+        if self.artist < other.artist:
+            return True
+        if self.artist == other.artist and self.album < other.album:
+            return True
+        return False
     
 def export_songs(songs, file_names):
     f = open(file_names, "w")
