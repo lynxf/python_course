b2afbea2518c30e6f5cec6097db4615fb14cadb9
lynxf <aleksandra9122@mail.ru>
2015-12-05 13:27:58 +0300

    Update task2.py

--- exam2/task2.py
+++ exam2/task2.py
@@ -1,1 +1,26 @@
-
+import re
+import requests
+with open("e2.txt", "r") as d:
+    l = d.read()
+    l = l.split("\n")
+    Lst = []
+    d.close()
+    for i in l:
+        t = i
+        response = requests.get(i)
+        html_code = response.text
+#print(html_code)
+        regex = '\w+@+\w+\.+\w+'
+        results = re.findall(regex, html_code)
+        Lst.append(results)
+u = []
+with open("bla-bla.txt", "w") as a:
+    for i in Lst:
+        for j in i:
+            u.append(j)
+d={}
+for i in u:
+    if i not in d:
+        d[i] = 1
+    else:
+        d[i] += 1
