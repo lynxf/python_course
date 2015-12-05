e84b7db7344d70f50ffdf91d8010a852888f8765
lynxf <aleksandra9122@mail.ru>
2015-12-05 13:32:19 +0300

    Update task2.py

--- exam2/task2.py
+++ exam2/task2.py
@@ -7,20 +7,25 @@ with open("e2.txt", "r") as d:
     d.close()
     for i in l:
         t = i
-        response = requests.get(i)
-        html_code = response.text
+        response=requests.get(i)
+        html_code=response.text
 #print(html_code)
-        regex = '\w+@+\w+\.+\w+'
-        results = re.findall(regex, html_code)
+        regex='\w+@+\w+\.+\w+'
+        results=re.findall(regex, html_code)
         Lst.append(results)
 u = []
 with open("bla-bla.txt", "w") as a:
     for i in Lst:
         for j in i:
             u.append(j)
-d={}
-for i in u:
-    if i not in d:
-        d[i] = 1
-    else:
-        d[i] += 1
+    d={}
+    for i in u:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
+
+    for i in d:
+        a.write(i)
+        a.write('\n')
+    a.close()
